8c67cf372e3686b652309cb5c217d0a19d9c5759
kws_kipis <kws@kipis.co.kr>
2021-02-21 02:23:40 +0900

    sequelize 복습

---
+++ sequelize/models/user.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
+        married: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false, // created_at 로 할지 createdAt으로 할지
+        modelName: "User", // javascript에서 쓰는 테이블명
+        tableName: "users", // 실제 sql 테이블명
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Comment, { foreignKey: "commenter", sourceKey: "id" });
+  }
+};
