60e1c42d64db5ef85048fd0d4263a38fe97b1ecf
kimwonseok <kws@kipis.co.kr>
2021-03-03 13:53:59 +0900

    웹소켓 완료

--- chat-gif/app.js
+++ chat-gif/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleWare = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -34,7 +34,8 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+})
+app.use(sessionMiddleWare);
 
 app.use((req, res, next) => {
   if (!req.session.color) {
@@ -47,7 +48,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
   next(error);
 });
@@ -63,4 +64,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleWare);
